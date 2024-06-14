64cd312253fa7178fef3db909534160e52c61959
Mausam <mvadakka@uwo.ca>
2024-06-13 22:48:54 -0400

    Initial commiy

---
+++ MV_Assignment_2.R
@@ -0,0 +1,10 @@
+#' Mausam Vadakkayil
+#' Assignment 2: Narcissistic Numbers
+
+#' Prompt the user to enter a three digit positive number
+
+#' Check if the user input is numeric. If not, print an error message and quit.
+
+#' Check if the number is narcissistic. A narcissistic number, or an Armstrong number, is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
+
+#' Display the result with an appropriate message, e.g. “127 is not an Armstrong number” or “370 is a narcissistic number” or any additional fun text you may want to add.
