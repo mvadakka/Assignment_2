d86f500a88914ec1dead7fcd283a09d625e6e7a7
Mausam <mvadakka@uwo.ca>
2024-06-17 18:33:51 -0400

    check if armstrong usinf conditionals (not working)

--- MV_Assignment_2.R
+++ MV_Assignment_2.R
@@ -1,18 +1,37 @@
 #' Mausam Vadakkayil
 #' Assignment 2: Narcissistic Numbers
 
+#'Plan
+#'If user.input is numeric, positive, and three digits
+#'subset digits of number
+#'else (cube first index and save to dig1 & cube second index and save to dig2 & cube third index and save to dig3
+#'and sum.digits <- sum(dig1 + dig2 + dig 3)
+#'if user.input == sum.digits
+#'print (user.input is an Armstrong number)
+#'else
+#'print (user.input is not an armstrong number)
+
+
 #' Prompt the user to enter a three digit positive number
 pos.num <- readline(prompt = "Please enter a three digit positive number:")
 numeric.pos <- as.numeric(pos.num)
 
-#' Check if the user input is numeric. If not, print an error 
-#' message and quit.
+#' Check if the user input is numeric. If not, print an error message and quit.
 if(is.na(numeric.pos) | nchar(pos.num) > 3 | pos.num < 0) { #first check if numeric.pos is NA (i.e. non numerical value), then that it does not have more than three digits and that it is not a negativie number
   print(paste("ERROR:", pos.num,"is not a valid three digit positive number.")) #if numeric.pos is NA, more than three digits and negative, print error and quit)
-}
+} 
+
+digits.pos <- (as.numeric(unlist(strsplit(pos.num, "")))) #if above conditions satisfied, subset digits of number and save in numeric form to digits.pos
 
 #' Check if the number is narcissistic. A narcissistic number, or an Armstrong number, 
 #' is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
 
+if((dig1 <- digits.pos[1]^3) & (dig2 <- digits.pos[2]^3) & (dig3 <- digits.pos[3]^3)) {
+  sum.digits <- sum(dig1, dig2, dig3)
+} else if (isTRUE(sum.digits == numeric.pos)) {
+  print(paste(numeric.pos, "is an Armstrong Number! Congratulations!"))
+} else {
+  print(paste(numeric.pos, "is not an Armstrong Number! Congratulations!"))
+}
 
 #' Display the result with an appropriate message, e.g. “127 is not an Armstrong number” or “370 is a narcissistic number” or any additional fun text you may want to add.
