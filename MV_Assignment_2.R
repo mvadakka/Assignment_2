b019ed207da15dcbc472ff75f2ba823da22c4c93
Mausam <mvadakka@uwo.ca>
2024-06-17 16:19:32 -0400

    checking if user input is numeric

--- MV_Assignment_2.R
+++ MV_Assignment_2.R
@@ -3,8 +3,10 @@
 
 #' Prompt the user to enter a three digit positive number
 
-#' Check if the user input is numeric. If not, print an error message and quit.
+#' Check if the user input is numeric. If not, print an error 
+#' message and quit.
 
-#' Check if the number is narcissistic. A narcissistic number, or an Armstrong number, is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
+#' Check if the number is narcissistic. A narcissistic number, 
+#' or an Armstrong number, is a number that is equal to the sum of the cubes of its own digits. 153, 370, 371, 407 are three digit Armstrong numbers.
 
 #' Display the result with an appropriate message, e.g. “127 is not an Armstrong number” or “370 is a narcissistic number” or any additional fun text you may want to add.
